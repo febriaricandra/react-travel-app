3a60dd5c1946c249b528b3752686eae32fcf73d7
febriaricandra <febriaricandraproduction@gmail.com>
2022-04-21 00:39:36 +0700

    add destionation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import beachVid from '../assets/beachVid.mp4'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 export default function Hero() {
   return (
@@ -8,7 +9,15 @@ export default function Hero() {
         <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
         <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white'>
             <h1>First class Travel</h1>
-            <h2>Top 1% Locations worldwide</h2>
+            <h2 className='py-4'>Top 1% Locations worldwide</h2>
+            <form className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90'>
+              <div>
+                <input className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none' type="text" placeholder='search destination' />
+              </div>
+              <div>
+                <button><AiOutlineSearch size={20} className='icon' style={{color: '#ffffff'}}/></button>
+              </div>
+            </form>
         </div>
     </div>
   )
