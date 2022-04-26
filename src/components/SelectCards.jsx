21fd6265884f7f7d82f63950e30c165653d4cabb
febriaricandra <febriaricandraproduction@gmail.com>
2022-04-26 12:22:49 +0700

    final intro

--- src/components/SelectCards.jsx
+++ src/components/SelectCards.jsx
@@ -6,7 +6,7 @@ export default function SelectCards({bg, text}) {
     <div className='relative '>
             <img src={bg} alt="/" className='w-full h-full object-cover' />
             <div className='bg-gray-900/30 absolute top-0 left-0 w-full h-full '>
-                <p className='left-4 bottom-4 text-2xl text-white font-bold'>{text}</p>
+                <p className='left-4 bottom-4 text-2xl text-white font-bold absolute'>{text}</p>
             </div>
     </div>
   )
